d5ac8e93df44bee02f00531059d9ff99aca00405
Vijay Bhagwandas <35529520+VBhagwandas@users.noreply.github.com>
2024-03-30 14:17:06 +0100

    Update and rename not_found_404.py to pages/not_found_404.py

--- pages/not_found_404.py
+++ pages/not_found_404.py
@@ -2,16 +2,12 @@ from dash import dcc, html, register_page
 import dash_bootstrap_components as dbc
 
 register_page(__name__, 
-              title = 'Tasting Intelligence App')
+              title = 'My App')
 
 not_found = dbc.Card(
     [
         dbc.CardBody(
             [
-                html.Div([
-                html.Div(html.Img(src="assets/tint-logo-wb-svg.svg", height='150'), style={'width':'100%', 'display': 'inline-block', 'textAlign': 'center'}),
-                ], style={'textAlign': 'center'}),
-                html.Br(),
                 dbc.Alert(html.H4("404: Not found"), color='danger'),
                 html.Div("The page you are looking for is not found."),
                 html.Br(),
