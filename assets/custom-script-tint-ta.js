55542f3847c9aa0dec041ad0f585706de91ad6b9
Vijay Bhagwandas <35529520+VBhagwandas@users.noreply.github.com>
2024-03-30 14:20:28 +0100

    Add files via upload

---
+++ assets/custom-script-tint-ta.js
@@ -0,0 +1,10 @@
+// //alert('If you see this alert, then your custom JavaScript script has run!')
+// Paddle.Environment.set('sandbox');
+// Paddle.Setup({ vendor: 3137 });
+
+//Google Analytics Tracker
+// window.dataLayer = window.dataLayer || [];
+// function gtag(){dataLayer.push(arguments);}
+// gtag('js', new Date());
+
+// gtag('config', 'G-PJ8QW6EZ81');
